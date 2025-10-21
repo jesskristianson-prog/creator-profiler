39f1cb9500270e0c257d81f48dfc660dddef7b93
jesskristianson-prog <jesskristianson@gmail.com>
2025-10-21 02:36:38 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from .database import Base
+from database import Base
 
 class CreatorJob(Base):
     __tablename__ = "creator_jobs"
